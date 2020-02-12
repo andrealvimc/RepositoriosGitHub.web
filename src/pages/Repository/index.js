2d3e662d946c3c091f96ebf271d5d2444952b907
André Alvim <andre@alvim.net>
2020-02-12 17:19:03 -0300

    Carregando dados da API repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      await api.get(`/repos/${repoName}`),
+      await api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5
+        }
+      })
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.date,
+      loading: false
+    });
+  }
+
+  render() {
+    const {repository, issues, loading} = this.state;
+
+    return <h1>Repository:</h1>;
+  }
 }
