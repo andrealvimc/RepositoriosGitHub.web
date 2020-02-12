6c14d99c38bf3ac09b6028bbc8b6e0bee755ed6d
André Alvim <andre@alvim.net>
2020-02-12 18:07:52 -0300

    Adicionado novas propriedades na página repository

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,8 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
-import { Container, Form, SubmitButton, List } from './styles';
+import { Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container/index';
 
 export default class Main extends Component {
   // eslint-disable-next-line react/state-in-constructor
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,10 +1,23 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/sort-comp */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
+import PropsType from 'prop-types';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Loading, Owner, IssuesList } from './styles';
+import Container from '../../components/Container/index';
 
 export default class Repository extends Component {
+  static propsType = {
+    match: PropsType.shape({
+      params: PropsType.shape({
+        repository: PropsType.string
+      })
+    }).isRequired
+  };
+
   state = {
     repository: {},
     issues: [],
@@ -17,8 +30,8 @@ export default class Repository extends Component {
     const repoName = decodeURIComponent(match.params.repository);
 
     const [repository, issues] = await Promise.all([
-      await api.get(`/repos/${repoName}`),
-      await api.get(`/repos/${repoName}/issues`, {
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
         params: {
           state: 'open',
           per_page: 5
@@ -28,14 +41,44 @@ export default class Repository extends Component {
 
     this.setState({
       repository: repository.data,
-      issues: issues.date,
+      issues: issues.data,
       loading: false
     });
   }
 
   render() {
-    const {repository, issues, loading} = this.state;
+    const { repository, issues, loading } = this.state;
 
-    return <h1>Repository:</h1>;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos Repositorios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
+      </Container>
+    );
   }
 }
